89602234a2fc261e5160f7fac3857ff88ccd3d13
venediktova-kate <venediktova.kat@gmail.com>
2023-02-28 17:48:06 +0700

    fix

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from config import Config
 from dao.model.director import Director
 from dao.model.genre import Genre
 from dao.model.movie import Movie
-from helpers.constants import DATA
 from setup_db import db
 from views.auth import auth_ns
 from views.directors import director_ns
@@ -29,47 +28,12 @@ def register_extensions(app):
     api.add_namespace(movie_ns)
     api.add_namespace(auth_ns)
     api.add_namespace(user_ns)
-    create_data(app, db)
 
 
-def create_data(app, db):
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
+app = create_app(Config())
 
-        data = DATA
-
-        for movie in data["movies"]:
-            m = Movie(
-                id=movie["pk"],
-                title=movie["title"],
-                description=movie["description"],
-                trailer=movie["trailer"],
-                year=movie["year"],
-                rating=movie["rating"],
-                genre_id=movie["genre_id"],
-                director_id=movie["director_id"],
-            )
-            db.session.add(m)
-            db.session.commit()
-
-        for director in data["directors"]:
-            d = Director(
-                id=director["pk"],
-                name=director["name"],
-            )
-            db.session.add(d)
-            db.session.commit()
-
-        for genre in data["genres"]:
-            d = Genre(
-                id=genre["pk"],
-                name=genre["name"],
-            )
-            db.session.add(d)
-            db.session.commit()
-
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    app = create_app(Config())
     app.run(debug=True)
